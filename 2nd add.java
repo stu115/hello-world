bfa44d06cd9469c81e7861d22558ef4d0d467192
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-02-28 23:12:47 +0000

    Add files via upload

---
+++ 2nd add.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Hello3 {
+
+	public static void main(String[]args)
+	{
+	 System.out.println(1+2);
+	}
+}
---
+++ Hello2.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Hello2 {
+	
+	public static void main(String[]args)
+	{
+	 System.out.println("Hello");
+	 
+	 System.out.println("UWL World");
+	}
+
+}
